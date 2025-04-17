74b83e687e910a18e4fe8a047b10bdd9a8c40ca0
Abd Ullah <46522854+abdullahshinwari@users.noreply.github.com>
2025-04-17 07:13:43 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use Strict" //treat all JS code as new version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3 + 
+    3) // code readability should be high
+console.log("hello")
+
+let name = 'Ahmed'
+let age = 25
+let isLoggedIN = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => '' or ""
+// boolean => true/ false
+// null => standalone value
+// undefine => undefine
+// symbol => use for uiqueness
+
+// object
+
+console.log(typeof null)
+console.log(typeof undefined)
